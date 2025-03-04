04d27bd568066d53c75e75de33a9c89fae3474d9
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-04 14:36:26 +0530

    oops design

---
+++ 04 - Object Oriented Design Principles/UniversityFacultiesDepartments/src/Department.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Department {
+    private String name;
+    private final ArrayList<Faculty> faculties;
+
+    public Department(String name) {
+        this.name = name;
+        faculties = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Faculty> getFaculties() {
+        return faculties;
+    }
+    public void addFaculty(Faculty faculty) {
+        faculties.add(faculty);
+    }
+}
---
+++ 04 - Object Oriented Design Principles/UniversityFacultiesDepartments/src/Faculty.java
@@ -0,0 +1,14 @@
+public class Faculty {
+    private String name;
+
+    public Faculty(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
